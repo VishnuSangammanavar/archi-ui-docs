b928ab44dff08064d5d7bb91b2b2eacec140d3a5
VishnuSangammanavar <vishnusangammanavar25@gmail.com>
2025-07-26 13:30:36 +0530

    code block update

--- src/demo/switch/IconSwitch.jsx
+++ src/demo/switch/IconSwitch.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Switch } from 'archi-ui'
+import { Moon, SunMedium } from 'lucide-react'
 
 const IconSwitch = () => {
   return (
-    <div className="flex items-center justify-center gap-2">
-      <Switch color='info' variant='standard' icon='l' checkedIcon='d' />
+    <div className="flex items-center justify-center">
+      <Switch color='info' variant='standard' icon={<Moon className='text-black' />} checkedIcon={<SunMedium className='text-black' />} />
     </div>
   )
 }
